1b1abfc59396336e4220aa340877998de831cf52
vikram-karunagaran <159704740+vikram-karunagaran@users.noreply.github.com>
2024-02-15 23:58:11 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,5 +5,5 @@ set -e
 docker pull vikram6/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 vikram6/simple-python-flask-app
+docker run -d -p 6000:6000 vikram6/simple-python-flask-app
 
