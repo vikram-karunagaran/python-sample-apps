3c10d7039f1127662701e5061335e3b81967d051
vikram-karunagaran <159704740+vikram-karunagaran@users.noreply.github.com>
2024-02-16 18:48:05 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-export cont_id=$(/usr/bin/docker ps | /usr/bin/awk -F " " '{print $1}' | /usr/bin/grep -v CONTAINER)
-/usr/bin/docker rm -f $cont_id
+/usr/bin/docker ps -q | /usr/bin/docker rm -f
+
